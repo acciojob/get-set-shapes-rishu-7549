acc7d3950fd06edbc02ca7474d2760facda96e74
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-04 11:53:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this.width = width;
+		this.height = height;
+	}
+	get width(){
+		return this.width;
+	}
+	get height(){
+		return this.height;
+	}
+	getArea(){
+		return width*height;
+	}
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+	constructor(side){
+		this.side = side;
+	}
+	super();
+	getPerimeter(){
+		return 4*side;
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
